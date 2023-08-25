027e69bab4ca5ef6ef29508a993310069946ac26
henryrneh <henryharry93@gmail.com>
2023-08-25 21:24:01 +0200

    Add example for minimize corpus

---
+++ minimize-corpus/src/explore_me.cpp
@@ -0,0 +1,66 @@
+#include "explore_me.h"
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+typedef unsigned char uchar;
+static const std::string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";//=
+
+static std::string base64_encode(const std::string &in) {
+    std::string out;
+
+    int val=0, valb=-6;
+    for (uchar c : in) {
+        val = (val<<8) + c;
+        valb += 8;
+        while (valb>=0) {
+            out.push_back(b[(val>>valb)&0x3F]);
+            valb-=6;
+        }
+    }
+    if (valb>-6) out.push_back(b[((val<<8)>>(valb+8))&0x3F]);
+    while (out.size()%4) out.push_back('=');
+    return out;
+}
+
+static std::string base64_decode(const std::string &in) {
+
+    std::string out;
+
+    std::vector<int> T(256,-1);
+    for (int i=0; i<64; i++) T[b[i]] = i;
+
+    int val=0, valb=-8;
+    for (uchar c : in) {
+        if (T[c] == -1) break;
+        val = (val<<6) + T[c];
+        valb += 6;
+        if (valb>=0) {
+            out.push_back(char((val>>valb)&0xFF));
+            valb-=8;
+        }
+    }
+    return out;
+}
+
+// just a function with multiple paths that can be discoverd by a fuzzer
+void exploreMe(int a, int b, string c) {
+  if (a >= 20000) {
+    if (b >= 2000000) {
+      if (b - a < 100000) {
+        // Trigger the undefined behavior sanitizer
+        int n = 23;
+        n <<= 32;
+
+        if (base64_encode(c) == "RlVaWklORw==") {
+          // Trigger a heap buffer overflow
+          char *s = (char *)malloc(1);
+          strcpy(s, "too long");
+          printf("%s\n", s);
+        }
+      }
+    }
+  }
+}
